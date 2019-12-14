10e3830ff7da6469d925dbc907f1d39ab455bb8b
Th3Shadowbroker <webmaster@m4taiori.de>
2019-12-14 11:59:08 +0100

    Added project files

---
+++ Server/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class User
+    {
+
+
+        public string Name { get; private set; }
+
+        public TcpClient Client { get; private set; }
+
+        public NetworkStream ClientStream { get; private set; }
+
+        private StreamWriter ClientWriter { get; set; }
+
+        private StreamReader ClientReader { get; set; }
+
+        public User(string name, TcpClient client)
+        {
+            this.Name = name;
+            this.Client = client;
+            this.ClientStream = client.GetStream();
+            this.ClientWriter = new StreamWriter(ClientStream);
+            this.ClientReader = new StreamReader(ClientStream);
+        }
+
+        public void Send(string message)
+        {
+            ClientWriter.Write(message);
+            ClientWriter.Flush();
+        }
+
+        public string Receive()
+        {
+            return ClientReader.ReadLine();
+        }
+
+        public void Disconnect()
+        {
+            this.ClientStream.Close();
+        }
+
+    }
+}
---
+++ Server/UserManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class UserManager
+    {
+
+        private List<User> users = new List<User>();
+
+        public void Register(User user)
+        {
+            users.Add(user);
+        }
+
+        public void Remove(User user)
+        {
+            users.Remove(user);
+        }
+
+        public User GetByAddress(IPAddress address)
+        {
+            return users.FirstOrDefault(usr => ((IPEndPoint)usr.Client.Client.RemoteEndPoint).Address == address);
+        }
+
+        public User GetByName(string name)
+        {
+            return users.FirstOrDefault(usr => usr.Name == name);
+        }
+
+        public void Broadcast(string message)
+        {
+            users.ForEach(usr =>
+            {
+                usr.Send("Server: " + message);
+            });
+        }
+
+        public void Send(User sender, string message)
+        {
+            sender.Send(sender.Name + ": " + message);
+        }
+
+    }
+}
